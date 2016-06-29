1f46ca892cf196eabf1c48c316298525076d4a39
Niall <noccer@gmail.com>
2016-06-30 00:39:13 +1000

    decided to make my colours using the time of day. made a new Date(), then set it to 00:00:00. I plan to increment the clock by 1 each time in a loop until I reach 23:59:59.

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ $(function() {
   var seconds = 0;
   var minutes = 0;
   var hours = 0;
-  var time = [hours,minutes,seconds];
+  var time = [hours, minutes, seconds];
 
-  var colourMaker = function (time){
+  var d = new Date();
+  d.setHours(0,0,0);
+  console.log(d);
+
+  var colourMaker = function(time) {
     // increase seconds by 1 so long as time is less than 24:00:00
     if (seconds < 60 && minutes < 60 && hours < 24) {
       seconds += 1;
     }
     // reset seconds back to 0 once it reaches 60
-    if (seconds === 60 && minutes < 60) {
+    if (seconds === 60) {
       seconds = 0;
-      minutes += 1; // add a minute on
+      if (minutes < 60) {
+        minutes += 1; // add a minute on
+      }
     }
     //
-    
+
   };
 
 
