2cfa6c1ebb4a74d49301338670c7c75534405e19
Niall <noccer@gmail.com>
2016-06-30 00:16:58 +1000

    created variables for hours mins seconds, and an array to hold them inside of. created a function to create a colour hash for every single second of the day

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 $(function() {
-    console.log( "ready!" );
+  console.log("jQueery Loaded!");
+
+  var seconds = 0;
+  var minutes = 0;
+  var hours = 0;
+  var time = [hours,minutes,seconds];
+
+  var colourMaker = function (time){
+
+  };
+
+
+
+
+
+
+
 });
