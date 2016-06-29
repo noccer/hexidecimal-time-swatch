cfbd0958f4109ad46a4fb483c43501b750125d98
Niall <noccer@gmail.com>
2016-06-30 00:48:32 +1000

    moved to a startTime and endTime concept. This way I can increment the time by 1, fetch the HH:MM:SS, then populate a #hexa colour to fill into my page. Also ran a test 'e' time stamp to increment the time by +1. Worked fine.

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 $(function() {
   console.log("jQueery Loaded!");
 
-  var seconds = 0;
-  var minutes = 0;
-  var hours = 0;
-  var time = [hours, minutes, seconds];
+  // var seconds = 0;
+  // var minutes = 0;
+  // var hours = 0;
+  // var time = [hours, minutes, seconds];
 
-  var d = new Date();
-  d.setHours(0,0,0);
-  console.log(d);
+  var startTime = new Date();
+  startTime.setHours(0,0,0);
+  console.log(startTime);
 
-  var colourMaker = function(time) {
-    // increase seconds by 1 so long as time is less than 24:00:00
-    if (seconds < 60 && minutes < 60 && hours < 24) {
-      seconds += 1;
-    }
-    // reset seconds back to 0 once it reaches 60
-    if (seconds === 60) {
-      seconds = 0;
-      if (minutes < 60) {
-        minutes += 1; // add a minute on
-      }
-    }
-    //
+  e = startTime;
+  e.setSeconds(e.getSeconds() + 1);
+  console.log(e);
 
-  };
+  var endTime = new Date();
+  endTime.setHours(23,59,59);
+  console.log(endTime);
+  // var colourMaker = function(time) {
+  //   // increase seconds by 1 so long as time is less than 24:00:00
+  //   if (seconds < 60 && minutes < 60 && hours < 24) {
+  //     seconds += 1;
+  //   }
+  //   // reset seconds back to 0 once it reaches 60
+  //   if (seconds === 60) {
+  //     seconds = 0;
+  //     if (minutes < 60) {
+  //       minutes += 1; // add a minute on
+  //     }
+  //   }
+  //   //
+  //
+  // };
 
 
 
