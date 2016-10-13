3eda169370de1f1de00310ed9d97d343bb5193ab
Niall <noccer@yahoo.ie>
2016-10-14 00:12:10 +1100

    remove console.logs().

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-  console.log("jQueery Loaded!");
 
   var startTime = new Date();
   startTime.setHours(0, 0, 0);
-  console.log(startTime);
 
   e = startTime;
   e.setSeconds(e.getSeconds() + 1);
-  console.log(e);
 
   var endTime = new Date();
   endTime.setHours(01, 01, 01);
-  console.log(endTime);
 
   currentTime = new Date();
   currentTime.setHours(0, 0, 0);
 
   var tickTock = function(){
-    console.log(currentTime);
     for (var i = 0; i < 10 ; i++) {
-      console.log(currentTime.getSeconds() + i);
     }
   };
   tickTock();
