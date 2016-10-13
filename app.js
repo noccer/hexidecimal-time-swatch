b7a584be0f7079734a1056085a0cb818bd1c4460
Niall <noccer@yahoo.ie>
2016-10-14 00:20:18 +1100

    Complete the program using pure javaScript :)

--- app.js
+++ app.js
@@ -9,8 +9,29 @@
   endTime.setHours(00, 59, 59);
 
   var tickTock = function(){
-    for (var i = 0; i < 10 ; i++) {
+    for (var i = startTime; i <= endTime; i.setSeconds(i.getSeconds() + 1)) {
+      var hours = "0" + i.getHours();
+      var minutes = "0" + i.getMinutes();
+      var seconds = "0" + i.getSeconds();
+      // substring so that we only take on the last 2 digits, as numbers > 9 will be 012, 034 etc.
+      var timeInHex = hours.substr(-2) + minutes.substr(-2) + seconds.substr(-2);
+
+      // create the div container and give it style/class
+      var colourDiv = document.createElement("div");
+
+      colourDiv.style.backgroundColor = '#' + timeInHex;
+      colourDiv.className += "divvy";
+
+      // create the span for showing the colour as text
+      var colourSpanText = document.createElement("span");
+      colourSpanText.className += "time";
+      colourSpanText.innerHTML = '#' + timeInHex;
+
+      // add the span to the div
+      colourDiv.appendChild(colourSpanText);
+
+      // add the div to the #colours div element
+      document.getElementById("colours").appendChild(colourDiv);
     }
   };
   tickTock();
-
